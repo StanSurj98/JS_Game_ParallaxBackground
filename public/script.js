33012c0c579eb16e370076e84a88d970cb349dc7
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-16 18:25:40 -0700

    add layer images and define initial scroll speed

--- public/script.js
+++ public/script.js
@@ -4,3 +4,6 @@ const ctx = canvas.getContext("2d");
 // Explicitly declare dimensions as same, else it defaults 
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
+
+// Next consider how we want dynamic background speeds depending on the character
+let gameSpeed = 5; 
