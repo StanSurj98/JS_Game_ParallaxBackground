5e0908132a2e0bb86452b6d7d726787af1e73039
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-16 18:39:21 -0700

    animate the layer moving to the left

--- public/script.js
+++ public/script.js
@@ -8,7 +8,7 @@ const CANVAS_HEIGHT = canvas.height = 700;
 // Next consider how we want dynamic background speeds depending on the character
 let gameSpeed = 5; 
 
-// Source the layers from the /images folder
+// Source the layers from the /images folder | all are H: 720px
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = './images/layer-1.png';
 const backgroundLayer2 = new Image();
@@ -20,12 +20,19 @@ backgroundLayer4.src = './images/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = './images/layer-5.png';
 
+
+// This controls our horizontal "animation"
+let x = 0
+
 // Next let's build our function that holds the animation logic
 const animate = () => {
+  // Clears the canvas frame at beginning of each iteration, no more "smudging" from frames being on top of each other
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-  // First let's draw the first layer, from top left canvas to bottom right. 
-  ctx.drawImage(backgroundLayer4, 0, 0);
-
+  // First let's draw a layer, from top left canvas and fill
+  ctx.drawImage(backgroundLayer4, x, 0);
+  // each loop we're drawing the image "x" px to left of canvas
+  x--; 
 
   // Recursion to create an animation effect
   requestAnimationFrame(animate);
