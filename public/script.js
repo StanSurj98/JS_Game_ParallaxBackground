96b4b15e98992b579eda04473d1c61c90aba134a
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-16 18:22:05 -0700

    define canvas dimensions

---
+++ public/script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("canvas1");
+// Need context to use drawing methods
+const ctx = canvas.getContext("2d"); 
+// Explicitly declare dimensions as same, else it defaults 
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
