7d63c853162d51c25e5ffb2c58bab49d6f6aad09
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-16 18:53:34 -0700

    basic method of stacking 2 images next to each other for continual scroll

--- public/script.js
+++ public/script.js
@@ -22,7 +22,9 @@ backgroundLayer5.src = './images/layer-5.png';
 
 
 // This controls our horizontal "animation"
-let x = 0
+let x = 0;
+let x2 = 2400;
+
 
 // Next let's build our function that holds the animation logic
 const animate = () => {
@@ -31,10 +33,16 @@ const animate = () => {
 
   // First let's draw a layer, from top left canvas and fill
   ctx.drawImage(backgroundLayer4, x, 0);
+  // This is to prevent big gap, we just stack another image in front of it
+  ctx.drawImage(backgroundLayer4, x2, 0);
   // let's reset the image after it scrolls -1000px pixels
-  if (x < -1000) x = 0; 
+  if (x < -2400) x = 2400; 
   // each loop we're drawing the image "x" px to left of canvas
   else x -= gameSpeed;
+  // Must reset the position of the 2nd image as well
+  if (x2 < -2400) x2 = 2400; 
+  // each loop we're drawing the image "x" px to left of canvas
+  else x2 -= gameSpeed;
 
   // Recursion to create an animation effect
   requestAnimationFrame(animate);
