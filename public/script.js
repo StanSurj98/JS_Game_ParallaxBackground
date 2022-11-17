e229cd7c25aae63f5a0f8bcf737b7f44ad39c225
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-17 13:59:58 -0700

    adjust speeds

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
 
 // Next consider how we want dynamic background speeds depending on the character
-let gameSpeed = 15; 
+let gameSpeed = 8; 
 
 // Source the layers from the /images folder | all are H: 720px
 const backgroundLayer1 = new Image();
@@ -62,11 +62,11 @@ class Layer {
 }
 
 // Now let's create instances of the layer classes
-const layer1 = new Layer(backgroundLayer1, 0.5);
-const layer2 = new Layer(backgroundLayer2, 0.1);
-const layer3 = new Layer(backgroundLayer3, 0.15); 
-const layer4 = new Layer(backgroundLayer4, 0.25); 
-const layer5 = new Layer(backgroundLayer5, 0.5); 
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6); 
+const layer4 = new Layer(backgroundLayer4, 0.8); 
+const layer5 = new Layer(backgroundLayer5, 1); 
 
 // Refactor and DRY up code - Group into an array
 const gameObjects = [layer1, layer2, layer3, layer4, layer5];
